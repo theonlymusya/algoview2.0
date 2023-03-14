eb80c5bb188ed0d4533b5a5129a4a7a72532d551
Tamara Gadieva <tamara.gadievaa@mail.ru>
2023-03-14 17:21:25 +0300

    add class GraphInfo

---
+++ graph_info.hpp
@@ -0,0 +1,23 @@
+#include <map>
+#include "argument.hpp"
+#include "block.hpp"
+#include "vertex.hpp"
+
+namespace graph_info {
+
+using CoordType = double;
+using ParamsMap = std::map<std::string, CoordType>;
+
+class GraphInfo {
+   public:
+    void add_param(std::string name, int value);
+    void print_params() const;
+    void print_graph() const;
+    ParamsMap& get_params();
+    BlockTagsInfo& get_blocks();
+
+   private:
+    ParamsMap params_;
+    BlockTagsInfo blocks_;
+};
+}  // namespace graph_info
