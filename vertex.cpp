2f6fb51bbfcc9132ce27b79e2f34c2eca5d52e1f
Tamara Gadieva <tamara.gadievaa@mail.ru>
2023-03-14 12:47:41 +0300

    modified class VertexTagsInfo

--- vertex.cpp
+++ vertex.cpp
@@ -2,40 +2,40 @@
 #include <iostream>
 
 namespace graph_info {
-void VertexTagsInfo::add_vertex(VertexType type, std::string cond, std::vector<std::string>& src) {
-    vertex_tags_.emplace_back(VertexTagInfo{type, cond});
-    for (auto src_elem : src) {
-        vertex_tags_[n_].src.push_back(src_elem);
-    }
+
+void VertexTagsInfo::new_vertex() {
+    vertex_tags_.emplace_back(VertexTagInfo{});
     n_++;
 }
 
-void VertexTagsInfo::add_vertex(VertexType type,
-                                std::string cond,
-                                std::vector<std::string>& src,
-                                std::vector<std::string>& bsrc) {
-    vertex_tags_.emplace_back(VertexTagInfo{type, cond});
-    for (auto src_elem : src) {
-        vertex_tags_[n_].src.push_back(src_elem);
-    }
-    for (auto bsrc_elem : bsrc) {
-        vertex_tags_[n_].bsrc.push_back(bsrc_elem);
-    }
-    n_++;
+void VertexTagsInfo::add_condition(std::string cond) {
+    vertex_tags_[n_].cond = cond;
+}
+
+void VertexTagsInfo::add_type(std::string type) {
+    vertex_tags_[n_].type = type;
+}
+
+void VertexTagsInfo::add_src(std::string src) {
+    vertex_tags_[n_].src.push_back(src);
+}
+
+void VertexTagsInfo::add_bsrc(std::pair<std::string, std::string> bsrc) {
+    vertex_tags_[n_].bsrc.push_back(bsrc);
 }
 
 void VertexTagsInfo::print_vertex_tags() const {
     for (const auto& vertex : vertex_tags_) {
         std::cout << "type = " << vertex.type << std::endl;
         std::cout << "condition = " << vertex.cond << std::endl;
-        std::cout << "src: " << std::endl;
-        for (const auto& src_elem : vertex.src) {
-            std::cout << "\t" << src_elem << std::endl;
+        if (!vertex.src.empty()) {
+            for (const auto& src_elem : vertex.src) {
+                std::cout << "src:\t" << src_elem << std::endl;
+            }
         }
         if (!vertex.bsrc.empty()) {
-            std::cout << "bsrc: " << std::endl;
             for (const auto& bsrc_elem : vertex.bsrc) {
-                std::cout << "\t" << bsrc_elem << std::endl;
+                std::cout << "bsrc:\t" << bsrc_elem.first << "\tsrc:\t" << bsrc_elem.second << std::endl;
             }
         }
         std::cout << std::endl;
--- vertex.hpp
+++ vertex.hpp
@@ -11,23 +11,21 @@ struct VertexTagInfo {
     VertexType type = "1";
     std::string cond = "";
     std::vector<std::string> src;
-    std::vector<std::string> bsrc;
-    // std::vector<std::pair<std::string, std::string>> bsrc;
+    std::vector<std::pair<std::string, std::string>> bsrc;
 };
 
 class VertexTagsInfo {
    public:
-    void add_vertex(VertexType type, std::string cond, std::vector<std::string>& src);
-    void add_vertex(VertexType type, std::string cond, std::vector<std::string>& src, std::vector<std::string>& bsrc);
     void new_vertex();
-    // void add_condtion();
-    // void add_type();
-    // void add_bsrc();
-    // void add_src();
+    void add_condition(std::string cond);
+    void add_type(std::string type);
+    void add_src(std::string src);
+    void add_bsrc(std::pair<std::string, std::string> bsrc);
+
     void print_vertex_tags() const;
 
    private:
-    int n_ = 0;
+    int n_ = -1;
     std::vector<VertexTagInfo> vertex_tags_;
 };
 }  // namespace graph_info
