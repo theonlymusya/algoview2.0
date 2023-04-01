ab007e407379b4bff101087b2d3b57611e11d2de
Tamara Gadieva <tamara.gadievaa@mail.ru>
2023-04-01 19:30:26 +0300

    modified parse_value function

--- parse_src_coords.cpp
+++ parse_src_coords.cpp
@@ -5,16 +5,19 @@ using CoordType = int;
 using namespace reg_expr;
 std::map<std::string, double> args_map;
 
-void parse_value(const std::string& str, std::string& left_value, std::string& right_value) {
-    int first_comma_pos = str.find(',');
-    char* c_left_value = (char*)malloc(first_comma_pos);
-    int left_value_len = str.copy(c_left_value, first_comma_pos, 0);
+void parse_value(const std::string& str, std::string& left_value, std::string& right_value, char parse_sym) {
+    int first_parse_sym_pos = str.find(parse_sym);
+    char* c_left_value = (char*)malloc(first_parse_sym_pos);
+    int left_value_len = str.copy(c_left_value, first_parse_sym_pos, 0);
     c_left_value[left_value_len] = '\0';
     left_value = std::string(c_left_value);
-    first_comma_pos++;
-    int right_value_len = str.length() - first_comma_pos;
+    if (parse_sym == ',')
+        first_parse_sym_pos++;
+    else if (parse_sym == '.')
+        first_parse_sym_pos += 2;
+    int right_value_len = str.length() - first_parse_sym_pos;
     char* c_right_value = (char*)malloc(right_value_len);
-    left_value_len = str.copy(c_right_value, right_value_len, first_comma_pos);
+    left_value_len = str.copy(c_right_value, right_value_len, first_parse_sym_pos);
     c_right_value[left_value_len] = '\0';
     right_value = std::string(c_right_value);
 }
@@ -26,13 +29,13 @@ void get_src_vertex_coords(const std::string& src_string, int dim, CoordType& i,
         j = -1;
         k = -1;
     } else if (dim == 2) {
-        parse_value(src_string, i_str, j_str);
+        parse_value(src_string, i_str, j_str, ',');
         i = calc_expr(i_str, args_map);
         j = calc_expr(j_str, args_map);
         k = -1;
     } else if (dim == 3) {
-        parse_value(src_string, i_str, jk_str);
-        parse_value(jk_str, j_str, k_str);
+        parse_value(src_string, i_str, jk_str, ',');
+        parse_value(jk_str, j_str, k_str, ',');
         i = calc_expr(i_str, args_map);
         j = calc_expr(j_str, args_map);
         k = calc_expr(k_str, args_map);
