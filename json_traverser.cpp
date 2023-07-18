795210d2e048cf26965b9e5b6b3a8db68e6a5e15
Tamara Gadieva <tamara.gadievaa@mail.ru>
2023-07-19 02:33:37 +0300

    added some loggers

--- json_traverser.cpp
+++ json_traverser.cpp
@@ -5,7 +5,10 @@
 #include "argument.hpp"
 #include "block.hpp"
 #include "expr.hpp"
+#include "logger.hpp"
 #include "vertex.hpp"
+
+const std::string file_name = "json_traverse.cpp";
 
 namespace algoview_json_traverser {
 
@@ -130,7 +133,9 @@ void JSON_Traverser::traverse_vertex_element(const Value& vertex_tag, GraphInfo&
 }
 
 void JSON_Traverser::traverse_vertex(const Value& vertex_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_vertex(const Value& vertex_tag) {
+    const std::string func_name = "traverse_vertex";
+    logger::log_file_enter(func_name, file_name);
+
     if (vertex_tag.IsArray()) {
         for (SizeType i = 0; i < vertex_tag.Size(); i++) {
             traverse_vertex_element(vertex_tag[i], graph);
@@ -142,120 +147,232 @@ void JSON_Traverser::traverse_vertex(const Value& vertex_tag, GraphInfo& graph) 
     } else {
         assert("Тег vertex имеет неправильный тип");
     }
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse_block_element(const Value& block_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_block_element(const Value& block_tag) {
-    assert(block_tag.IsObject() && "Тег block не является объектом");
+    const std::string func_name = "traverse_block_element";
+    logger::log_file_enter(func_name, file_name);
+    // assert(block_tag.IsObject() && "Тег block не является объектом");
+    if (!block_tag.IsObject()) {
+        logger::log_err_msg(func_name, file_name, "Block tag is not an Object");
+        exit(1);
+    }
     if (block_tag.HasMember("id"))
-        assert(block_tag["@id"].IsString());
-    assert(block_tag.HasMember("@dims") && "Не указана размерность пространства");
-    assert(block_tag["@dims"].IsString());
+        // assert(block_tag["@id"].IsString());
+        if (!block_tag["@id"].IsString()) {
+            logger::log_err_msg(func_name, file_name, "Id attr is not a string");
+            exit(1);
+        }
+    // assert(block_tag.HasMember("@dims") && "Не указана размерность пространства");
+    if (!block_tag.HasMember("@dims")) {
+        logger::log_err_msg(func_name, file_name, "Dimentional not specified");
+        exit(1);
+    }
+    // assert(block_tag["@dims"].IsString());
+    if (!block_tag["@dims"].IsString()) {
+        logger::log_err_msg(func_name, file_name, "Dimentional is not a string");
+        exit(1);
+    }
     BlockTagsInfo& blocks = graph.get_blocks();
     blocks.new_block();
     for (Value::ConstMemberIterator itr = block_tag.MemberBegin(); itr != block_tag.MemberEnd(); ++itr) {
         if (!strcmp(itr->name.GetString(), "@id")) {
-            assert(block_tag["@id"].IsString());
+            // assert(block_tag["@id"].IsString());
+            logger::log_char_msg("Found block id = ", block_tag["@id"].GetString());
             blocks.add_id(std::stoi(block_tag["@id"].GetString()));
-            printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            logger::log_info_msg("Added id to internal block structure");
+            // printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
         } else if (!strcmp(itr->name.GetString(), "@dims")) {
-            assert(block_tag["@dims"].IsString());
+            // assert(block_tag["@dims"].IsString());
+            if (!block_tag["@dims"].IsString()) {
+                exit(1);
+            }
+            logger::log_info_msg("Found tag dim");
             blocks.add_dim(std::stoi(block_tag["@dims"].GetString()));
-            printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            logger::log_info_msg("Added dim to internal block structure");
+            // printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
         } else if (!strcmp(itr->name.GetString(), "arg")) {
-            printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            // printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            logger::log_info_msg("Found tag arg");
             traverse_arg(itr->value, graph);
-            // traverse_arg(itr->value);
         } else if (!strcmp(itr->name.GetString(), "vertex")) {
-            printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            // printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            logger::log_info_msg("Found tag vertex");
             traverse_vertex(itr->value, graph);
-            // traverse_vertex(itr->value);
         } else {
-            assert("Неопознанный тег внутри block");
+            // assert("Неопознанный тег внутри block");
+            logger::log_err_msg(func_name, file_name, "Undefined tag inside block tag");
+            exit(1);
         }
     }
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse_block(const Value& block_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_block(const Value& block_tag) {
+    const std::string func_name = "traverse_block";
+    logger::log_file_enter(func_name, file_name);
+    logger::log_info_start_msg("traversing blocks");
+
     if (block_tag.IsArray()) {
         for (SizeType i = 0; i < block_tag.Size(); i++) {
             traverse_block_element(block_tag[i], graph);
-            // traverse_block_element(block_tag[i]);
         }
     } else if (block_tag.IsObject()) {
         traverse_block_element(block_tag, graph);
-        // traverse_block_element(block_tag);
     } else {
-        assert("Тег block имеет неправильный тип");
+        // assert("Тег block имеет неправильный тип");
+        logger::log_err_msg(func_name, file_name, "Block tag has undefined type");
+        exit(1);
     }
+
+    logger::log_info_finish_msg("traversing blocks");
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse_param_element(const Value& param_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_param_element(const Value& param_tag) {
-    assert(param_tag.IsObject() && "Тег param не является объектом");
-    assert(param_tag.HasMember("@name") && "Не указано имя параметра");
-    assert(param_tag["@name"].IsString());
-    printf("name = %s\n", param_tag["@name"].GetString());
-    assert(param_tag.HasMember("@type") && "Не указан тип параметра");
-    assert(param_tag["@type"].IsString());
-    printf("type = %s\n", param_tag["@type"].GetString());
+    const std::string func_name = "traverse_param_element";
+    logger::log_file_enter(func_name, file_name);
+    logger::log_info_start_msg("handling parameter");
+
+    // assert(param_tag.IsObject() && "Тег param не является объектом");
+    if (!param_tag.IsObject()) {
+        logger::log_err_msg(func_name, file_name, "Param tag is not an Object");
+        exit(1);
+    }
+    // assert(param_tag.HasMember("@name") && "Не указано имя параметра");
+    // *for user
+    if (!param_tag.HasMember("@name")) {
+        logger::log_err_msg(func_name, file_name, "Parameter has no name");
+        exit(1);
+    }
+    // assert(param_tag["@name"].IsString());
+    // *for user
+    if (!param_tag["@name"].IsString()) {
+        logger::log_err_msg(func_name, file_name, "Parameter's name is not a string");
+        exit(1);
+    }
+    // printf("name = %s\n", param_tag["@name"].GetString());
+    // assert(param_tag.HasMember("@type") && "Не указан тип параметра");
+    // *for user
+    if (!param_tag.HasMember("@type")) {
+        logger::log_err_msg(func_name, file_name, "Parameter has no type");
+        exit(1);
+    }
+    // assert(param_tag["@type"].IsString());
+    // *for user
+    if (!param_tag["@type"].IsString()) {
+        logger::log_err_msg(func_name, file_name, "Parameter's type is not a string");
+        exit(1);
+    }
+    // printf("type = %s\n", param_tag["@type"].GetString());
+
     // if (param_tag.HasMember("@value")) {
     //     assert(param_tag["@value"].IsString());
     //     printf("val = %s\n", param_tag["@value"].GetString());
     // }
-    assert(param_tag.HasMember("@value"));
-    assert(param_tag["@value"].IsString());
-    printf("val = %s\n", param_tag["@value"].GetString());
+
+    // assert(param_tag.HasMember("@value"));
+    //*for user
+    if (!param_tag.HasMember("@value")) {
+        logger::log_err_msg(func_name, file_name, "Parameter has no value");
+        exit(1);
+    }
+    // assert(param_tag["@value"].IsString());
+    if (!param_tag["@value"].IsString()) {
+        logger::log_err_msg(func_name, file_name, "Parameter's value is not a string");
+        exit(1);
+    }
+    // printf("val = %s\n", param_tag["@value"].GetString());
     graph.add_param(param_tag["@name"].GetString(), std::stoi(param_tag["@value"].GetString()));
+
+    logger::log_info_finish_msg("handling parameter");
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse_param(const Value& param_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_param(const Value& param_tag) {
+    const std::string func_name = "traverse_param";
+    logger::log_file_enter(func_name, file_name);
+
     if (param_tag.IsArray()) {
         for (SizeType i = 0; i < param_tag.Size(); i++) {
             traverse_param_element(param_tag[i], graph);
-            // traverse_param_element(param_tag[i]);
         }
     } else if (param_tag.IsObject()) {
         traverse_param_element(param_tag, graph);
-        // traverse_param_element(param_tag);
     } else {
-        assert("Тег param имеет неправильный тип");
+        // assert("Тег param имеет неправильный тип");
+        logger::log_err_msg(func_name, file_name, "Param tag has undefined type");
+        exit(1);
     }
+
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse_params(const Value& params_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_params(const Value& params_tag) {
-    assert(params_tag.IsObject() && "Тег params не является объектом");
-    assert(params_tag.HasMember("param") && "Не указаны параметры");
+    const std::string func_name = "traverse_params";
+    logger::log_file_enter(func_name, file_name);
+    logger::log_info_start_msg("traversing parameters");
+
+    // assert(params_tag.IsObject() && "Тег params не является объектом");
+    if (!params_tag.IsObject()) {
+        logger::log_err_msg(func_name, file_name, "Params tag is not an Object");
+        exit(1);
+    }
+    // assert(params_tag.HasMember("param") && "Не указаны параметры");
+    // *for user
+    if (!params_tag.HasMember("param")) {
+        logger::log_err_msg(func_name, file_name, "Params tag does not contain parameter's definitions");
+        exit(1);
+    }
     traverse_param(params_tag["param"], graph);
-    // traverse_param(params_tag["param"]);
+
+    logger::log_info_finish_msg("traversing parameters");
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse_algo(const Value& algo_tag, GraphInfo& graph) {
-    // void JSON_Traverser::traverse_algo(const Value& algo_tag) {
+    const std::string func_name = "traverse_algo";
+    logger::log_file_enter(func_name, file_name);
+
+    logger::log_info_start_msg("traversing tags inside algo tag");
     for (Value::ConstMemberIterator itr = algo_tag.MemberBegin(); itr != algo_tag.MemberEnd(); ++itr) {
         if (!strcmp(itr->name.GetString(), "params")) {
-            printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            // printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            logger::log_info_msg("Found tag params");
             traverse_params(itr->value, graph);
-            // traverse_params(itr->value);
         } else if (!strcmp(itr->name.GetString(), "block")) {
-            printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            // printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+            logger::log_info_msg("Found tag block");
             traverse_block(itr->value, graph);
-            // traverse_block(itr->value);
         } else {
-            assert("Неопознанный тег внутри algo");
+            // assert("Неопознанный тег внутри algo");
+            // *for user
+            logger::log_err_msg(func_name, file_name, "Undefined tag inside algo tag");
+            exit(1);
         }
     }
+    logger::log_info_finish_msg("traversing tags inside algo tag");
+
+    logger::log_file_exit(func_name, file_name);
 }
 
 void JSON_Traverser::traverse(const Document& doc, GraphInfo& graph) {
     // void JSON_Traverser::traverse(const Document& doc) {
+    const std::string func_name = "parse_json_to_Document";
+    logger::log_file_enter(func_name, file_name);
+    logger::log_info_start_msg("traversing DOM tree");
+
     const Value& head_tag = doc["algo"];
-    assert(head_tag.IsObject() && "Тег algo не является объектом");
+    // assert(head_tag.IsObject() && "Тег algo не является объектом");
+    if (!head_tag.IsObject()) {
+        logger::log_err_msg(func_name, file_name, "Algo tag is not an Object");
+        exit(1);
+    }
     traverse_algo(head_tag, graph);
-    // traverse_algo(head_tag);
+
+    logger::log_info_finish_msg("traversing DOM tree");
+    logger::log_file_exit(func_name, file_name);
 }
 
 }  // namespace algoview_json_traverser
--- logger.hpp
+++ logger.hpp
@@ -10,19 +10,32 @@ const std::string func_out_log = "Exit function ";
 const std::string file_log = " In file ";
 const std::string func_log = "In func ";
 
-void log_file_enter(const std::string& func_name, const std::string file_name) {
+inline void log_file_enter(const std::string& func_name, const std::string& file_name) {
     std::cerr << info_log << func_in_log << func_name << file_log << file_name << std::endl;
 }
 
-void log_file_exit(const std::string& func_name, const std::string file_name) {
+inline void log_file_exit(const std::string& func_name, const std::string& file_name) {
     std::cerr << info_log << func_out_log << func_name << file_log << file_name << std::endl;
 }
 
-void log_info_msg(const std::string& msg) {
+inline void log_info_msg(const std::string& msg) {
     std::cerr << info_log << msg << std::endl;
 }
 
-void log_err_msg(const std::string& func_name, const std::string file_name, const std::string& msg) {
+inline void log_info_start_msg(const std::string& msg) {
+    std::cerr << info_log << "Start " << msg << std::endl;
+}
+
+inline void log_info_finish_msg(const std::string& msg) {
+    std::cerr << info_log << "Finish " << msg << " sucessfully!" << std::endl;
+}
+
+inline void log_char_msg(const std::string& msg, const char* detail) {
+    std::cerr << info_log << msg;
+    fprintf(stderr, "%s\n", detail);
+}
+
+inline void log_err_msg(const std::string& func_name, const std::string file_name, const std::string& msg) {
     std::cerr << err_log << func_log << func_name << file_log << file_name << ": " << msg << std::endl;
 }
 }  // namespace logger
