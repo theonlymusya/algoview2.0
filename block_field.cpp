3984754857c27c6e9cc311f6a2a5e656f0ecc3ea
Tamara Gadieva <tamara.gadievaa@mail.ru>
2023-09-12 12:15:38 +0300

    fixed critical length

--- block_field.cpp
+++ block_field.cpp
@@ -16,6 +16,7 @@ using namespace logger;
 using namespace output_file_manager;
 using VarsMap = std::map<std::string, double>;
 int cur_block_shift = 0;
+int i_o_vertex_flag = 0;
 
 Block::Block(const BlockTagInfo& _block_info, int _block_number) {
     const auto& args = _block_info.get_args();
@@ -118,6 +119,8 @@ VertexId Block::create_vertex(VertexMapManager& vertices_manager,
         new Vertex{block_id, i + i_shift_, j + j_shift_, k + k_shift_ + (cur_block_shift - local_block_shift_), type};
     if (type != "0")
         graph_charact_manager.inc_vertices_counter();
+    else
+        i_o_vertex_flag = 1;
     VertexId new_vertex_id = vertices_manager.add_vertex(new_vertex_ptr);
     logger.log_info_msg("New vertex = " + std::to_string(new_vertex_id));
 
@@ -410,7 +413,10 @@ void Block::main_cycle(const BlockTagInfo& block_info,
                             // if vertex has level = 0 these vertices are ignored, since level = 0 is not
                             // considered to be a part of the graph (extra part)
                             graph_charact_manager.inc_level_vertex_counter(level);
-                            graph_charact_manager.add_critical_lenght(level - 1);
+                            if (i_o_vertex_flag)
+                                graph_charact_manager.add_critical_lenght(level - 1);
+                            else
+                                graph_charact_manager.add_critical_lenght(level);
                             logger.log_info_finish_msg("determining current vertex level");
                         }
                     }
